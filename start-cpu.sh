74c75fbcdb44a5a8002f7673e8d1a886a953745f
abeiro <augusto.beiro@gmail.com>
2025-11-16 16:09:26 +0100

    Change directory before starting CPU script

--- start-cpu.sh
+++ start-cpu.sh
@@ -1,1 +1,2 @@
-/home/dwemer/parakeet-api-server/start_native.sh --cpu  --precision int8
+cd /home/dwemer/parakeet-api-server/
+/home/dwemer/parakeet-api-server/start_native.sh --cpu  --precision int8 &> log.txt&
