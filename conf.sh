6e0181844af420042181a2b613057a545e5c7564
RANGROO <reecemeakings@gmail.com>
2025-11-27 09:46:11 -0800

    parakeet conf

--- conf.sh
+++ conf.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 clear
 cat << EOF
-parakeet
+Parakeet STT
 
-TO-DO: Explain here
+This will configure the Parakeet TTS (Text-to-Speech) service..
+
+Options:
+* GPU = Uses GPU acceleration for faster inference. Recommended for NVIDIA cards.
+* CPU = Runs on CPU only. Use this for AMD cards or systems without GPU support.
+
+Recommended to use GPU if you have a Nvidia GPU.
 
 EOF
 
-echo "0. Disable service";
+echo "Select an option from the list:"
+echo
 echo "1. Enable service (GPU)"
 echo "2. Enable service (CPU)"
+echo "0. Disable service"
+echo
 
 # Prompt the user to make a selection
 read -p "Select an option by picking the matching number: " selection
@@ -17,7 +26,7 @@ read -p "Select an option by picking the matching number: " selection
 # Validate the input
 
 if [ "$selection" -eq "0" ]; then
-    echo "Disabling service. Run this again to enable"
+    echo "Disabling service. Run this again to enable it"
     rm /home/dwemer/parakeet-api-server/start.sh 
     exit 1
 fi
